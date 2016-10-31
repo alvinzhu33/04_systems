6e73a0c64271f366c5cb24746f298ce53b8b503a
Alvin Zhu <alvinzhu33@yahoo.com>
2016-10-31 15:16:24 -0400

    Size ok probably

--- main.c
+++ main.c
@@ -3,16 +3,39 @@
 #include <sys/stat.h>
 #include <time.h>
 
+void easySize(int size){
+  printf("Size: ");
+  if(size >= 1000000000){
+    printf("%d GB\n", size/1000000000);
+  }else{
+    if(size >= 1000000){
+      printf("%d MB\n", size/1000000);
+    }else{
+      if(size >= 1000){
+	printf("%d KB\n", size/1000);
+      }else{
+	printf("%d B\n", size/1);
+      }
+    }
+  }
+}
+
+void easyPermissions(int mode){
+  printf("%d\n",mode);
+}
+
 int main(){
-    //making stat
-    struct stat info;
-    stat("main.c", &info);
+  //making stat
+  struct stat info;
+  stat("main.c", &info);
 
-    //getting info (thanks Internet!)
-    printf("Info for main.c:\n");
-    printf("Size: %lu bytes\n", info.st_size);
-    printf("Mode: %o\n", info.st_mode);
-    printf("Access: %s\n", ctime(&info.st_atime));
+  //getting info (thanks Internet!)
+  printf("Info for main.c:\n");
+  easySize(info.st_size);
+  printf("Mode: %o\n", info.st_mode);
+  int try=info.st_mode;
+  easyPermissions(try);
+  printf("Access: %s\n", ctime(&info.st_atime));
 
-    return 0;
+  return 0;
 }
