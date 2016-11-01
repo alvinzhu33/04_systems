7c82e460f51149f7addef9774913d457030fbaf9
Alvin Zhu <alvinzhu33@yahoo.com>
2016-11-01 15:07:57 -0400

    Done

--- main.c
+++ main.c
@@ -27,19 +27,27 @@ void easyPermissions(int mode){
     perm[i] = mode%8;
     mode=mode/8;
   }
-  /*perm[5] = mode%8;
-  mode = mode/8;
-  perm[4] = mode%8;
-  mode = mode/8;
-  perm[3] = mode%8;
-  mode = mode/8;
-  perm[2] = mode%8;
-  mode = mode/8;
-  perm[1] = mode%8;
-  mode = mode/8;
-  perm[0] = mode%8;
-  mode = mode/8;*/
-  printf("%d%d%d%d%d%d\n", perm[0], perm[1], perm[2], perm[3], perm[4], perm[5]);
+  i = 3;
+  printf("-");
+  for(i; i<6; i++){
+    if(perm[i]>=4){
+      printf("r");
+    }else{
+      printf("-");
+    }
+    if(perm[i]==2 || perm[i]==3 || perm[i]==6 || perm[i]==7){
+      printf("w");
+    }else{
+      printf("-");
+    }
+    if(perm[i]%2){
+      printf("x");
+    }else{
+      printf("-");
+    }
+  }
+  printf("\n");
+  //printf("%d%d%d%d%d%d\n", perm[0], perm[1], perm[2], perm[3], perm[4], perm[5]);
 }
 
 int main(){
@@ -50,7 +58,8 @@ int main(){
   //getting info (thanks Internet!)
   printf("Info for main.c:\n");
   easySize(info.st_size);
-  printf("Mode: %o\n", info.st_mode);
+
+  printf("Mode: ");
   easyPermissions(info.st_mode);
   printf("Access: %s\n", ctime(&info.st_atime));
 
